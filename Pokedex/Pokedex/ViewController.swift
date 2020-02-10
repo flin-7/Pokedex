990af7b550ea61a71749e13d9533444e35a1a43f
flin-7 <flin.7@icloud.com>
2020-02-09 22:04:22 -0800

    Initial Project Setup - No Storyboard

---
+++ Pokedex/Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Felix Lin on 2/9/20.
+//  Copyright © 2020 Felix Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
